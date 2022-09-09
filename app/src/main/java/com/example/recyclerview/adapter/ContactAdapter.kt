529b8e84ab80b0e03758747b6b75eead23395186
lusiro <lusiro2001@students.amikom.ac.id>
2022-09-09 21:48:03 +0700

    second

--- app/src/main/java/com/example/recyclerview/adapter/ContactAdapter.kt
+++ app/src/main/java/com/example/recyclerview/adapter/ContactAdapter.kt
@@ -13,19 +13,19 @@ import com.example.recyclerview.data.MyContact
 class ContactAdapter(private val listContact: ArrayList<MyContact>): RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
 
     // for diffutil
-    private val diffCallBack = object : DiffUtil.ItemCallback<MyContact>(){
-        override fun areItemsTheSame(oldItem: MyContact, newItem: MyContact): Boolean {
-            return oldItem.nama == newItem.nama
-        }
-
-        override fun areContentsTheSame(oldItem: MyContact, newItem: MyContact): Boolean {
-            return oldItem.hashCode() == newItem.hashCode()
-        }
-    }
-
-    private val differ = AsyncListDiffer(this, diffCallBack)
-
-    fun submitData(value: ArrayList<MyContact>) = differ.submitList(value)
+//    private val diffCallBack = object : DiffUtil.ItemCallback<MyContact>(){
+//        override fun areItemsTheSame(oldItem: MyContact, newItem: MyContact): Boolean {
+//            return oldItem.nama == newItem.nama
+//        }
+//
+//        override fun areContentsTheSame(oldItem: MyContact, newItem: MyContact): Boolean {
+//            return oldItem.hashCode() == newItem.hashCode()
+//        }
+//    }
+//
+//    private val differ = AsyncListDiffer(this, diffCallBack)
+//
+//    fun submitData(value: ArrayList<MyContact>) = differ.submitList(value)
 
     // class Holder
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
@@ -43,12 +43,15 @@ class ContactAdapter(private val listContact: ArrayList<MyContact>): RecyclerVie
     // melakukan penentuan data yang akan ditampilkan pada setiap item atau baris
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        val data = differ.currentList[position]
+//        val data = differ.currentList[position]
 
         holder.nama.text = listContact[position].nama
         holder.noHp.text = listContact[position].noHp
     }
 
     // memberitahu banyaknya data yang ditampilkan
-    override fun getItemCount(): Int = differ.currentList.size
+    override fun getItemCount(): Int{
+        return listContact.size
+    }
 }
+//= differ.currentList.size
